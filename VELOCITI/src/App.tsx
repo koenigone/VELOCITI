e454b2753170406d194f9f2cf1ad15e8c1e6e3fa
koenigone <mm6dd.mh@gmail.com>
2026-02-05 15:04:06 +0000

    initialized VALOCITI react+vite project

---
+++ VELOCITI/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+      </div>
+      <h1>VALOCITI: The Future Accelerated</h1>
+      <div className="card">
+        <p>
+          This is the start of VELOCITI train project
+        </p>
+      </div>
+      <p className="read-the-docs">
+        In preperation stage..
+      </p>
+    </>
+  )
+}
+
+export default App
