80273711eef826ef4d6cdaeb335a4cefb76c9f56
NRazmo <nickrazmo@gmail.com>
2026-02-16 22:12:16 +0000

    Added station selection logic and route polyline rendering

--- src/components/map/mapArea.tsx
+++ src/components/map/mapArea.tsx
@@ -1,26 +1,42 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useState } from "react";
+import 
+{
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Polyline,
+} 
+from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 import stationDataRaw from "../../data/TiplocPublicExport_2025-12-01_094655.json";
 
-type Tiploc = {
+type Tiploc = 
+{
   Name: string;
   Tiploc: string;
   Latitude: number;
   Longitude: number;
+  Details: 
+  {
+    CRS: string | null;
+  };
 };
 
-type TiplocExport = {
+type TiplocExport = 
+{
   ExportDate: string;
   ExportCount: number;
   Tiplocs: Tiploc[];
 };
 
-const stationData = stationDataRaw as TiplocExport;
+const stationData: TiplocExport = stationDataRaw as TiplocExport;
 
 delete (L.Icon.Default.prototype as any)._getIconUrl;
-L.Icon.Default.mergeOptions({
+L.Icon.Default.mergeOptions
+({
   iconRetinaUrl:
     "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
   iconUrl:
@@ -29,8 +45,39 @@ L.Icon.Default.mergeOptions({
     "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
 });
 
-export default function MapArea() {
-  const stations = stationData.Tiplocs;
+export default function MapArea() 
+{
+  const stations: Tiploc[] = stationData.Tiplocs.filter
+  (
+    (station) => station.Details?.CRS !== null
+  );
+
+  const [startStation, setStartStation] = useState<Tiploc | null>(null);
+  const [endStation, setEndStation] = useState<Tiploc | null>(null);
+
+  const handleStationClick = (station: Tiploc) => 
+   {
+    if (!startStation) 
+    {
+      setStartStation(station);
+    } 
+    else if (!endStation) 
+    {
+      setEndStation(station);
+    } else 
+    {
+      setStartStation(station);
+      setEndStation(null);
+    }
+   };
+
+  const routePositions: [number, number][] =
+    startStation && endStation
+      ? [
+          [startStation.Latitude, startStation.Longitude],
+          [endStation.Latitude, endStation.Longitude],
+        ]
+      : [];
 
   return (
     <MapContainer
@@ -43,21 +90,34 @@ export default function MapArea() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      {stations.slice(0, 300).map((station, index) => (
+      {stations.slice(0, 500).map((station, index) => (
         <Marker
           key={index}
           position={[station.Latitude, station.Longitude]}
+          eventHandlers=
+          {{
+            click: () => handleStationClick(station),
+          }}
         >
           <Popup>
             <strong>{station.Name}</strong>
+            <br />
+            CRS: {station.Details.CRS}
             <br />
             TIPLOC: {station.Tiploc}
           </Popup>
         </Marker>
       ))}
+
+      {routePositions.length === 2 && 
+      (
+        <Polyline positions={routePositions} />
+      )}
     </MapContainer>
   );
 }
+
+
 
 
 
